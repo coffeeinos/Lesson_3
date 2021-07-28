666031999a198f5c9b934c366a2fdd8af8a86b4c
coffeeinos <72664986+coffeeinos@users.noreply.github.com>
2021-07-28 19:40:43 +0300

    Add files via upload

---
+++ ConsoleAppLesson_3/ConsoleAppLesson_3/Program.cs
@@ -0,0 +1,25 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLesson_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int userNumber = Int32.Parse(Console.ReadLine());
+            if (userNumber % 2 == 0)
+            {
+                Console.WriteLine("Your number is even");
+            }
+            else
+            {
+                Console.WriteLine("Your number isn't even");
+            }
+        }
+    }
+}
